7b7a42acb49e34e742ddd99bffee826c79f6af25
Vinicius Lidington <vinicius.lins@encora.com>
2026-02-11 13:35:54 -0300

    add caching on project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import EventCard from '@/components/EventCard'
 import Explorebtn from '@/components/Explorebtn'
 import { IEvent } from '@/database/event.model'
+import { cacheLife } from 'next/cache'
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 const Page = async () => {
+  "use cache";
+  cacheLife('hours')
+  
   const response = await fetch(`${BASE_URL}/api/events`)
   const {events} = await response.json();
 
